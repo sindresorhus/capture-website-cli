8a916d32ba9e2fcb1913a4aad5b3fb9abdd168f0
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-03 15:33:50 +0100

    Update test.js
    Co-Authored-By: timoschwarzer <timoschwarzer@users.noreply.github.com>

--- test.js
+++ test.js
@@ -40,7 +40,7 @@ test('check flags', async t => {
 --user-agent="I love unicorns"
 --cookie="id=unicorn; Expires=Wed, 21 Oct 2018 07:28:00 GMT;"
 --authentication="username:password"
---launch-options="{ \\"headless\\": false }"
+--launch-options="{\\"headless\\": false}"
 	`;
 
 	flags = flags.trim()
